26decef030856f3262de80c9c1ac0f015b3571bc
Francieli Viane <francielifrv@gmail.com>
2017-05-28 17:41:53 -0300

    Enables JPA repositories

--- src/main/java/com/acme/configuration/DatabaseConfiguration.java
+++ src/main/java/com/acme/configuration/DatabaseConfiguration.java
@@ -1,13 +1,17 @@
 package com.acme.configuration;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableJpaRepositories("com.acme.repositories")
+@EntityScan("com.acme")
 public class DatabaseConfiguration {
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
