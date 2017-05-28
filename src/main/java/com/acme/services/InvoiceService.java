24b8a3cfbe678bed480579f2072ec741b60fb6f4
Francieli Viane <francielifrv@gmail.com>
2017-05-28 17:32:45 -0300

    Adds Service annotation to InvoiceService class

--- src/main/java/com/acme/services/InvoiceService.java
+++ src/main/java/com/acme/services/InvoiceService.java
@@ -7,7 +7,9 @@ import com.acme.repositories.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InvoiceService {
 
     private final InvoiceRepository repository;
