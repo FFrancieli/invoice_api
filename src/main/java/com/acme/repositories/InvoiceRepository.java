0a052daf48ea0b440c8967447413ea4d36f6266c
Francieli Viane <francielifrv@gmail.com>
2017-05-30 01:18:45 -0300

    Removes unnecessary repository annotation

--- src/main/java/com/acme/repositories/InvoiceRepository.java
+++ src/main/java/com/acme/repositories/InvoiceRepository.java
@@ -2,8 +2,6 @@ package com.acme.repositories;
 
 import com.acme.models.Invoice;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface InvoiceRepository extends CrudRepository<Invoice, Long>, InvoiceRepositoryCustom {
 }
